a2f8443e6163754cff289b02b58ac2226ddde274
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-09-10 15:41:13 -0500

    Se realizó la consulta del servicio que contiene el top 10 de los juegos en twitch

--- DaggerLogin/app/src/main/java/com/shell/android/daggerlogin/http/TwitchAPI.kt
+++ DaggerLogin/app/src/main/java/com/shell/android/daggerlogin/http/TwitchAPI.kt
@@ -3,9 +3,10 @@ package com.shell.android.daggerlogin.http
 import com.shell.android.daggerlogin.http.twitch.TopGameResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface TwitchAPI {
 
     @GET("games/top")
-    fun getTopGames(): Call<TopGameResponse>
+    fun getTopGames(@Header("Client-Id") clientId: String): Call<TopGameResponse>
 }
